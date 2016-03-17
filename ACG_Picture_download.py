8817b4fb50f6428b6d1e16a8cdba1268c23af3a0
小小酥XXS <syjsu@qq.com>
2016-03-17 10:36:58 +0800

    加入下载文件夹封面
    加入下载文件夹封面

--- ACG_Picture_download.py
+++ ACG_Picture_download.py
@@ -8,29 +8,33 @@ import time
 from  threading import Timer
 import re
 
+
 # #建立线程池，并启动线程直到结束
 def parallel(infos):
     startTime = time.time()
-    threads=[]
+    threads = []
     counts = range(len(infos))
     for i in counts:
-        t=MyThread(downloadImage,(infos[i],),downloadImage.__name__)
+        t = MyThread(downloadImage, (infos[i],), downloadImage.__name__)
         threads.append(t)
     for i in counts:
         threads[i].start()
     for i in counts:
         threads[i].join()
-    print('花费时间:%s'%(time.time()-startTime))
+    print('花费时间:%s' % (time.time() - startTime))
+
 
 # #自定义线程类
 class MyThread(threading.Thread):
-    def __init__(self,func,args,name=''):
+    def __init__(self, func, args, name=''):
         threading.Thread.__init__(self)
-        self.name=name
-        self.func=func
-        self.args=args
+        self.name = name
+        self.func = func
+        self.args = args
+
     def run(self):
-        self.res=self.func(*self.args)
+        self.res = self.func(*self.args)
+
 
 # # #单线程测试
 # def parallel(infos):
@@ -39,7 +43,7 @@ class MyThread(threading.Thread):
 #         downloadImage(infos[i])
 #     print("OK")
 
-#根绝imageUrl下载图片到本地
+# 根绝imageUrl下载图片到本地
 def downloadImage(info):
     imageUrl = info["url"]
     imagePName = str(info["pname"])
@@ -47,20 +51,20 @@ def downloadImage(info):
 
     print(imagePName)
 
-    imagePName = re.sub("[\s+\.\!\/_,$%^*+\"\']+|[+——！，。？、~@#￥%……&*（）]+", "-",imagePName)
+    imagePName = re.sub("[\s+\.\!\/_,$%^*+\"\']+|[+——！，。？、~@#￥%……&*（）]+", "-", imagePName)
 
-    dir = "./"+imagePName+"/"
+    dir = "./" + imagePName + "/"
 
     try:
         if not os.path.exists(dir):
             os.mkdir(dir)
-            print("创建目录成功 %s"%dir)
+            print("创建目录成功 %s" % dir)
     except:
-        print("创建目录失败 %s"%dir)
-        pass
+        print("创建目录失败 %s" % dir)
+        return
 
     imageType = imageUrl.split('.')[-1]
-    path = dir+imageCName + "."+imageType
+    path = dir + imageCName + "." + imageType
     if os.path.exists(path):
         print("文件已存在")
     else:
@@ -69,22 +73,26 @@ def downloadImage(info):
             data = urllib.request.urlopen(imageUrl).read()
         except:
             print("下载失败")
+            return
         try:
-            f = open(path,"wb")
+            f = open(path, "wb")
             f.write(data)
             f.close()
-            print("保存成功"+path)
+            print("保存成功" + path)
         except:
             print("保存失败")
+            return
 
-#下载整个相册
-def downloadAlbum(albumName,albumIndex):
-    reqs =[]
+
+# 下载整个相册
+def downloadAlbum(albumName, albumIndex):
+    reqs = []
     try:
-        res = urllib.request.urlopen("http://acgstay.mavericks.lol:8888/index?index="+albumIndex+"&mode=3")
+        res = urllib.request.urlopen("http://acgstay.mavericks.lol:8888/index?index=" + albumIndex + "&mode=3")
         resAlbum = json.loads(res.read().decode('utf-8'))
     except:
         print("获取专辑信息失败")
+        return
     for Album in resAlbum['info']:
         # print("文件名：" + Album['tags'] + " 文件ID：" + str(Album['id']) + " 下载地址：" + Album['url'])
         a = {}
@@ -94,7 +102,8 @@ def downloadAlbum(albumName,albumIndex):
         reqs.append(a)
     parallel(reqs)
 
-#获取专辑信息
+
+# 获取专辑信息
 def downloadIndex():
     try:
         res = urllib.request.urlopen("http://acgstay.mavericks.lol:8888/index?index=mainindex&mode=3")
@@ -102,19 +111,28 @@ def downloadIndex():
         print(resAlbum)
     except:
         print("获取索引失败")
+        return
     for Album in resAlbum['indexes']:
-        print("开始下载专辑" + Album['des'] +" 索引是：" + Album['index'])
-        downloadAlbum(Album['des'],Album['index'])
+        print("开始下载专辑" + Album['des'] + " 索引是：" + Album['index'])
+        # 下载专辑封面信息
+        pic = {}
+        pic['pname'] = '封面'
+        pic['cname'] = Album['des']
+        pic['url'] = Album['url']
+        downloadImage(pic)
+        # 下载专辑
+        downloadAlbum(Album['des'], Album['index'])
     print("结束任务")
+
 
 if __name__ == "__main__":
 
     timeout = 10
     socket.setdefaulttimeout(timeout)
     downloadIndex()
-    timer_interval=240
-    t=Timer(timer_interval,downloadIndex())
+    timer_interval = 240
+    t = Timer(timer_interval, downloadIndex())
     t.start()
     while True:
-        pass
+        time.sleep(0.1)
     print("结束")
