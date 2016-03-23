f03333ba33b820eccc893e3ef928c0c6376d6681
小小酥XXS <syjsu@qq.com>
2016-03-23 11:59:28 +0800

    新增对HTTPS的支持
    新增对HTTPS的支持

--- ACG_Picture_download.py
+++ ACG_Picture_download.py
@@ -16,7 +16,7 @@ def parallel(infos):
     threads=[]
     counts = range(len(infos))
     for i in counts:
-        t=MyThread(downloadImage,(infos[i],),downloadImage.__name__)
+        t=MyThread(downloadImage,(infos[i],downloadFile,),downloadImage.__name__)
         threads.append(t)
     for i in counts:
         threads[i].start()
@@ -35,14 +35,14 @@ class MyThread(threading.Thread):
         self.res=self.func(*self.args)
 
 # # #单线程测试
-# def parallel(infos):
+# def parallel(infos,downloadFile):
 #     counts = range(len(infos))
 #     for i in counts:
-#         downloadImage(infos[i])
+#         downloadImage(infos[i],downloadFile)
 #     print("OK")
 
 #根绝imageUrl下载图片到本地
-def downloadImage(info):
+def downloadImage(info,downloadFile):
     imageUrl = info["url"]
     imagePName = str(info["pname"])
     imageCName = str(info["cname"])
@@ -50,8 +50,6 @@ def downloadImage(info):
     print(imagePName)
 
     imagePName = re.sub("[\s+\.\!\/_,$%^*+\"\']+|[+——！，。？、~@#￥%……&*（）]+", "-",imagePName)
-
-    global downloadFile
 
     dir = "./"+downloadFile+"/"+imagePName+"/"
 
@@ -72,8 +70,8 @@ def downloadImage(info):
         print("文件不存在")
         try:
             data = urllib.request.urlopen(imageUrl).read()
-        except:
-            print("下载失败")
+        except Exception as e:
+            print("下载失败",e)
             return
         try:
             f = open(path,"wb")
@@ -85,9 +83,8 @@ def downloadImage(info):
             return
 
 #下载整个相册
-def downloadAlbum(albumName,albumIndex):
+def downloadAlbum(albumName,albumIndex,downloadUrl):
     reqs =[]
-    global downloadUrl
     try:
         res = urllib.request.urlopen(downloadUrl+"/index?index="+albumIndex+"&mode=3")
         resAlbum = json.loads(res.read().decode('utf-8'))
@@ -104,14 +101,23 @@ def downloadAlbum(albumName,albumIndex):
     parallel(reqs)
 
 # 获取专辑信息
-def downloadIndex():
-    global downloadUrl
+def downloadIndex(downloadUrl,downloadFile):
+    fileFolder="./"+downloadFile+"/"
+    if not os.path.exists(fileFolder):
+        os.mkdir(fileFolder)
+    timeout = 10
+    socket.setdefaulttimeout(timeout)
+
     try:
+
+        print(downloadUrl+"/index?index=mainindex&mode=3")
+
+        res = urllib.request.Request()
         res = urllib.request.urlopen(downloadUrl+"/index?index=mainindex&mode=3")
         resAlbum = json.loads(res.read().decode('utf-8'))
         print(resAlbum)
-    except:
-        print("获取索引失败")
+    except Exception as e:
+        print("获取索引失败",e)
         return
     for Album in resAlbum['indexes']:
         print("开始下载专辑" + Album['des'] + " 索引是：" + Album['index'])
@@ -120,32 +126,28 @@ def downloadIndex():
         pic['pname'] = '封面'
         pic['cname'] = Album['des']
         pic['url'] = Album['url']
-        downloadImage(pic)
+        downloadImage(pic,downloadFile)
         # 下载专辑
-        downloadAlbum(Album['des'], Album['index'])
+        downloadAlbum(Album['des'], Album['index'],downloadUrl)
     print("结束任务")
 
-# #下载地址
-# downloadUrl = "http://acgstay.mavericks.lol:8888"
-# #下载文件夹
-# downloadFile= "ACG"
 
-#下载地址
-downloadUrl = "http://cosplay.mavericks.lol:8887"
-#下载文件夹
-downloadFile= "COS"
 
 # 主程序
 if __name__ == "__main__":
 
-    fileFolder="./"+downloadFile+"/"
-    if not os.path.exists(fileFolder):
-        os.mkdir(fileFolder)
+    #下载地址
+    downloadUrl = "http://acgstay.mavericks.lol:8888"
+    #下载文件夹
+    downloadFile= "ACG"
 
-    timeout = 10
-    socket.setdefaulttimeout(timeout)
+    # #下载地址
+    # downloadUrl = "http://cosplay.mavericks.lol:8887"
+    # #下载文件夹
+    # downloadFile= "COS"
+
     timer_interval=240
-    t=Timer(timer_interval,downloadIndex())
+    t=Timer(timer_interval,downloadIndex(downloadUrl,downloadFile))
     t.start()
     while True:
         pass
